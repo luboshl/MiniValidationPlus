9eef66f0d8a181e2c036118aa114d0c5775dc867
Lubos Hladik <lubos@luboshladik.cz>
2024-07-15 00:26:35 +0200

    Add NonNullablePropertyHelper with tests

---
+++ src/MiniValidation/NonNullablePropertyHelper.cs
@@ -0,0 +1,31 @@
+﻿#if NET6_0_OR_GREATER
+
+using System.Reflection;
+
+namespace MiniValidation
+{
+    /// <summary>
+    /// Helper for non-nullable reference types.
+    /// </summary>
+    public static class NonNullablePropertyHelper
+    {
+        private static readonly NullabilityInfoContext NullabilityContext = new ();
+
+        /// <summary>
+        /// Gets information whether the <paramref name="propertyInfo"/> is non-nullable reference type.
+        /// </summary>
+        /// <param name="propertyInfo">The property.</param>
+        /// <returns><c>True</c> when <paramref name="propertyInfo"/> is non-nullable reference type, <c>False</c> otherwise.</returns>
+        public static bool IsNonNullableReferenceType(PropertyInfo propertyInfo)
+        {
+            if (propertyInfo.PropertyType.IsValueType)
+            {
+                return false;
+            }
+
+            var nullabilityInfo = NullabilityContext.Create(propertyInfo);
+            return nullabilityInfo.WriteState is not NullabilityState.Nullable;
+        }
+    }
+}
+#endif
